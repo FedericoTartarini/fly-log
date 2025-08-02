ece1407b8ce97b045a276d6eee06c159b272a309
federico tartarini <federicotartarini@gmail.com>
2025-08-02 21:41:57 +1000

    build: added mantine

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import "@mantine/core/styles.css";
+
+import {
+  createTheme,
+  MantineProvider,
+  Title,
+  ColorSchemeScript,
+} from "@mantine/core";
+
 import "./App.css";
 import WorldMap from "./components/WorldMap.jsx";
 import StatsSummary from "./components/StatsSummary.jsx";
 
+const theme = createTheme({
+  headings: {
+    fontFamily: "Roboto, sans-serif",
+  },
+});
+
 function App() {
   return (
     <>
-      <h1>Flight Tracker</h1>
-      <StatsSummary />
-      <WorldMap />
+      <ColorSchemeScript defaultColorScheme="auto" />
+      <MantineProvider theme={theme} defaultColorScheme="auto">
+        <Title>Flight Tracker</Title>
+        <StatsSummary />
+        <WorldMap />
+      </MantineProvider>
     </>
   );
 }
