338704046c9002032bc29c859c83e1e09d4f96ce
federico tartarini <federicotartarini@gmail.com>
2025-08-04 21:55:33 +1000

    lint: added some types.js

--- src/types.js
+++ src/types.js
@@ -19,4 +19,40 @@
  * @property {string} [aircraft_type_name]
  */
 
+/**
+ * @typedef {object} AirportTableRow
+ * @property {Date} departure_date
+ * @property {Date} departure_time
+ * @property {string} departure_airport_iata
+ * @property {string} arrival_airport_iata
+ * @property {string} airline_iata
+ * @property {string} flight_number
+ */
+
+/**
+ * @typedef {object} Airport
+ * @property {string} iata
+ * @property {string} icao
+ * @property {string} airport_name
+ * @property {string} country
+ * @property {string} city
+ * @property {number} lat
+ * @property {number} lon
+ * @property {number} elevation_ft
+ * @property {string} iso_country
+ * @property {string} iso_region
+ */
+
+/**
+ * @typedef {object} Airline
+ * @property {string} iata
+ * @property {string} icao
+ * @property {string} name
+ * @property {string} country
+ * @property {boolean} flag_carrier
+ * @property {string} website
+ * @property {string} alliance
+ * @property {object} branding
+ */
+
 export {}; // This file is only for type definitions.
