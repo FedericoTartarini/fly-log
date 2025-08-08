afdb352fe57657989fee35f93372aaa4b40200b9
federico tartarini <federicotartarini@gmail.com>
2025-08-08 22:54:15 +1000

    refactor(types): Update Flight type to FlightStats
    Revise the Flight type definition to better represent flight statistics,
    including properties for airlines, airports, and flight counts.

--- src/types.js
+++ src/types.js
@@ -1,22 +1,18 @@
 // src/types.js
 
 /**
- * @typedef {object} Flight
- * @property {string} date
- * @property {[number, number]} departure_coordinates
- * @property {[number, number]} arrival_coordinates
- * @property {number} [distance_km]
- * @property {number} [flight_time]
- * @property {string} [from]
- * @property {string} [to]
- * @property {string} [airline]
- * @property {string} [airline_icon_path]
- * @property {string} [airline_primary_color]
- * @property {string} [airline_name]
- * @property {string} [flight_number]
- * @property {string} [departure_country]
- * @property {string} [arrival_country]
- * @property {string} [aircraft_type_name]
+ * @typedef {object} FlightStats
+ * @property {number} [airlines]
+ * @property {number} [airports]
+ * @property {number} [countries]
+ * @property {number} [domesticFlights]
+ * @property {number} [internationalFlights]
+ * @property {number} [longHaulFlights]
+ * @property {object} [longestFlight]
+ * @property {object} [shortestFlight]
+ * @property {number} [totalDistance]
+ * @property {number} [totalFlightTime]
+ * @property {number} [westBoundFlights]
  */
 
 /**
