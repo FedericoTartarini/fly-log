5e8c8568c608780f6a2a0ddb9756ad7a25cea2ca
federico Tartarini <federicotartarini@gmail.com>
2026-01-18 14:16:09 +1100

    feat(Flights): fetch flights data on component mount using useEffect

--- src/pages/Flights.jsx
+++ src/pages/Flights.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Title, Stack, Card, Image } from "@mantine/core";
 import FlightYearFilter from "../components/FlightYearFilter";
 import flightImg from "../assets/flight.jpg";
 import FlightsList from "../components/FlightsList.tsx";
 import { useTranslation } from "react-i18next";
+import useFlightStore from "../store.ts";
 
 function Flights() {
   const { t } = useTranslation("flights");
+  const fetchFlights = useFlightStore((state) => state.fetchFlights);
+
+  useEffect(() => {
+    fetchFlights();
+  }, [fetchFlights]);
+
   return (
     <Container mt="md">
       <Stack spacing="xl">
