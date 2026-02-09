a0ec26c6eadc7d287b99ddbbec45ca603c2fe0e1
Federico Tartarini <{{ .email }}>
2026-02-09 20:09:34 +1100

    feat(config): update terser options to use pure_funcs for console logging removal

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,7 @@ export default defineConfig({
     minify: "terser",
     terserOptions: {
       compress: {
-        drop_console: true,
-        drop_debugger: true,
+        pure_funcs: ["console.log", "console.debug"],
       },
     },
   },
