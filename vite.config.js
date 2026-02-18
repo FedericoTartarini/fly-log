272428b5f9c7ce8135fc0d0b156fa769e00cf63a
Federico Tartarini <{{ .email }}>
2026-02-18 18:58:26 +1100

    feat(vite.config.js): replace VitePWA with vite-plugin-static-copy for service worker handling

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "sw.js",
-      injectManifest: {
-        swSrc: "src/sw.js",
-        swDest: "dist/sw.js",
-      },
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/sw.js",
+          dest: "",
+        },
+      ],
     }),
   ],
   build: {
