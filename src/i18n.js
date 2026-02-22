26f3f850cbe4969ba1403632bc4a13d4e2587487
Federico Tartarini <{{ .email }}>
2026-02-22 18:56:14 +1100

    feat(i18n): add new namespaces for flights, about, and landing

--- src/i18n.js
+++ src/i18n.js
@@ -12,7 +12,7 @@ i18n
     supportedLngs: ["en", "it"],
     fallbackLng: "en",
     debug: false,
-    ns: ["common", "login"],
+    ns: ["common", "login", "flights", "about", "landing"],
     defaultNS: "common",
     backend: {
       loadPath: "/locales/{{lng}}/{{ns}}.json",
