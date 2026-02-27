89ee546a70db9b8136a1e0925df28ac770c4fd1e
Federico Tartarini <{{ .email }}>
2026-02-28 09:49:51 +1100

    fix(sw): simplify caching logic by removing method check for GET requests

--- src/sw.js
+++ src/sw.js
@@ -95,7 +95,7 @@ self.addEventListener("fetch", (event) => {
       fetch(event.request)
         .then((fetchResponse) => {
           // Firestore returns POST for most structured queries, so we only cache GET responses.
-          if (fetchResponse.status === 200 && event.request.method === "GET") {
+          if (fetchResponse.status === 200) {
             return caches.open(DYNAMIC_CACHE).then((cache) => {
               cache.put(event.request, fetchResponse.clone());
               return fetchResponse;
